cadcfebee38986eef0507e5445e675d7d8e81497
AEX\AlexV <alexv@automationexchange.co.za>
2024-01-08 20:26:32 +0200

    Initial Commit

---
+++ DerivcoAssessment/ProblemSolvers/Abstract/ISolveProblems.cs
@@ -0,0 +1,6 @@
+﻿namespace DerivcoAssessment.ProblemSolvers.Abstract;
+
+public interface ISolveProblems<out T>
+{
+    public T Solve(string input);
+}
---
+++ DerivcoAssessment/ProblemSolvers/ProblemB.cs
@@ -0,0 +1,11 @@
+﻿using DerivcoAssessment.ProblemSolvers.Abstract;
+
+namespace DerivcoAssessment.ProblemSolvers;
+
+public class ProblemB : ISolveProblems<string>
+{
+    public string Solve(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
