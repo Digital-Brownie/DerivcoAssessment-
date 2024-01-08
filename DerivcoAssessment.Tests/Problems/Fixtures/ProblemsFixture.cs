852ae47903f91fe5d722321c4984b398b4af1f01
AEX\AlexV <alexv@automationexchange.co.za>
2024-01-08 20:36:10 +0200

    Add Tests

---
+++ DerivcoAssessment.Tests/Problems/Fixtures/ProblemsFixture.cs
@@ -0,0 +1,11 @@
+﻿using DerivcoAssessment.ProblemSolvers;
+using DerivcoAssessment.ProblemSolvers.Abstract;
+
+namespace DerivcoAssessment.Tests.Problems.Fixtures;
+
+public class ProblemsFixture
+{
+    public ISolveProblems<string> ProblemA { get; set; } = new ProblemA();
+    public ISolveProblems<string> ProblemB { get; set; } = new ProblemB();
+    public ISolveProblems<string> ProblemC { get; set; } = new ProblemC();
+}
---
+++ DerivcoAssessment.Tests/Problems/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using DerivcoAssessment.Tests.Problems.Fixtures;
+
+namespace DerivcoAssessment.Tests.Problems;
+
+public class UnitTests : IClassFixture<ProblemsFixture>
+{
+    private readonly ProblemsFixture _fixture;
+
+    public UnitTests(ProblemsFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void ProblemA()
+    {
+
+    }
+}
